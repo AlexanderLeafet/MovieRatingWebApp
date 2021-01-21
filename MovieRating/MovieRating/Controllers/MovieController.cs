ec31d77401153cef9807f8cbf03b127b35072a9e
AlexanderLeafet <alexander.lovdahl@live.se>
2021-01-21 20:18:10 +0100

    Remade project from scratch

---
+++ MovieRating/MovieRating/Controllers/MovieController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieRating.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult NineteenSeventeen()
+        {
+            return View();
+        }
+
+        public ActionResult LordOfTheRings()
+        {
+            return View();
+        }
+
+        public ActionResult Soul()
+        {
+            return View();
+        }
+
+        public ActionResult Tenet()
+        {
+            return View();
+        }
+
+        public ActionResult Joker()
+        {
+            return View();
+        }
+    }
+}
