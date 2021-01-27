18a8099beb2a411678ba47901f5c6d02e1454335
AlexanderLeafet <alexander.lovdahl@live.se>
2021-01-27 11:19:21 +0100

    Added datasource as JsonData file.

--- MovieRating/MovieRating/Controllers/HomeController.cs
+++ MovieRating/MovieRating/Controllers/HomeController.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MovieRating.Models;
+using Newtonsoft.Json;
 
 namespace MovieRating.Controllers
 {
     public class HomeController : Controller
     {
+        
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult GetMovieList()
+        {
+            var json = System.IO.File.ReadAllText(Server.MapPath(@"~/Content/JsonData.json"));
+            List<MovieInfo> movieInfoList = JsonConvert.DeserializeObject<List<MovieInfo>>(json);
+            
+            return Json(movieInfoList, JsonRequestBehavior.AllowGet);
         }
 
     }
